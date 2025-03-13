b18998af86563817566f599987265dc510e1f4d3
Igor <igorcarvalho288@gmail.com>
2025-03-13 19:39:02 -0300

    feat:"table styling and finalization of information obtained from json through axios"

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,22 @@
-fetch ('pessoas.json')
-.then(response => response.json())
-.then(json => carregarTabela(json));
+axios('pessoas.json')
+  .then(resposta => carregarTabela(resposta.data))
 
 
+function carregarTabela(json) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = ''; // Limpa antes de inserir novos dados
 
+  for (let pessoa of json) {
+    const tr = document.createElement('tr');
 
+    tr.innerHTML = `
+      <td>${pessoa.nome}</td>
+      <td>${pessoa.idade}</td>
+      <td>${pessoa.email}</td>
+      <td>${pessoa.empresa}</td>
+      <td>${pessoa.cpf}</td>
+    `;
 
-
+    resultado.appendChild(tr);
+  }
+}
